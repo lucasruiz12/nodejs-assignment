6de8cf0aacb4a844e73f7453aa93b15b9a44994f
Lucas Ruiz <79382758+lucasruiz12@users.noreply.github.com>
2021-11-18 17:18:28 -0300

    finish

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -5,27 +5,27 @@
 // output: [{ team: 'green', points: 40 }, ...]
 
 const players = require('../src/__tests__/__data__/players.json')
-const teamList = {}
+const teamList = {}  // initial object
 
 const calculateTeamPoints = () => {
-    for (let i = 0; i < players.length; i++) {
-      if (players[i].isActive && !teamList[players[i].team]) {
-        teamList[players[i].team] = {
-          team: players[i].team,
-          points: players[i].points
-        }
+  for (let i = 0; i < players.length; i++) {
+    if (players[i].isActive && !teamList[players[i].team]) {
+      teamList[players[i].team] = {  // adding team
+        team: players[i].team,
+        points: players[i].points
       }
-      else if (players[i].isActive && teamList[players[i].team]) {
-        teamList[players[i].team].points += players[i].points
-      }
-
     }
-    const response = Object.values(teamList)
+    else if (players[i].isActive && teamList[players[i].team]) {  // adding points
+      teamList[players[i].team].points += players[i].points
+    }
+  }
+  const response = Object.values(teamList)  // object to array
 
-    response.sort((a, b) => {
-      if (a.points < b.points) return 1;
-      if (a.points > b.points) return -1;
-    })
+  response.sort((a, b) => {  // sorting array
+    if (a.points < b.points) return 1;
+    if (a.points > b.points) return -1;
+  })
+  console.log("Respuesta", response)
   return response
 }
 
