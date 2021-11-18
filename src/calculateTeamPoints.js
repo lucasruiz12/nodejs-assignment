67626d0644efaf29655c7e92ea2edace2da5a024
Lucas Ruiz <79382758+lucasruiz12@users.noreply.github.com>
2021-11-18 12:02:02 -0300

    function ok, testing in progress

--- src/calculateTeamPoints.js
+++ src/calculateTeamPoints.js
@@ -3,6 +3,32 @@
 // The output is sorted in descending order by points
 // input: [{ team: 'green', name: 'Bob', points: 5, isActive: true }, ...]
 // output: [{ team: 'green', points: 40 }, ...]
-module.exports = (players) => {
-  return []
+
+const players = require('../src/__tests__/__data__/players.json')
+const teamList = {}
+
+const calculateTeamPoints = () => {
+    for (let i = 0; i < players.length; i++) {
+      if (players[i].isActive && !teamList[players[i].team]) {
+        teamList[players[i].team] = {
+          team: players[i].team,
+          points: players[i].points
+        }
+      }
+      else if (players[i].isActive && teamList[players[i].team]) {
+        teamList[players[i].team].points += players[i].points
+      }
+
+    }
+    const response = Object.values(teamList)
+
+    response.sort((a, b) => {
+      if (a.points < b.points) return 1;
+      if (a.points > b.points) return -1;
+    })
+  return response
+}
+
+module.exports = {
+  calculateTeamPoints
 }
